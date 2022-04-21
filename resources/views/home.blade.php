8d2e2fd747bcc8e6b734c3ad1b9e515fa2720798
coszach@hotmail.it <coszach@hotmail.it>
2022-04-21 15:51:01 +0200

    added style

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,19 +4,29 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="{{asset('css/app.css')}}">
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
   <title>laravel_model_controller</title>
 </head>
 <body>
-  
-  <ul>
-    @foreach($movies as $movie)
-    <div class="card-wrapper">
-      <li>
-        {{$movie['title']}}
-      </li>
+  <div class="container-fluid">
+    <div class="container">
+      <div class="card-wrapper">
+        @foreach($movies as $movie)
+          <div class="card">
+            <ul>
+              <li>
+                <h2 class="title">{{$movie['title']}}</h2>
+                <h4 class="original-title"> {{$movie['original_title']}} </h4>
+                <p>date: {{$movie['date']}}</p>
+                <p>vote: {{$movie['vote']}} </p>
+              </li>
+            </ul>
+          </div>
+        @endforeach
+      </div>
     </div>
-    @endforeach
-  </ul>
+  </div>
 
 </body>
 </html>
